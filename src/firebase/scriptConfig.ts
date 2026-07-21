d3fc5adbff1aaabbd003c71ba1f98f05cc05207e
apurvkolte10 <apurvkolte10@gmail.com>
2026-07-21 09:31:59 +0530

    ds

--- src/firebase/scriptConfig.ts
+++ src/firebase/scriptConfig.ts
@@ -3,13 +3,13 @@ import { getFirestore } from "firebase/firestore";
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCoJOme_Oto_WiL6Os8ZYciQndsmp55ir0",
-    authDomain: "sgs-ro.firebaseapp.com",
-    projectId: "sgs-ro",
-    storageBucket: "sgs-ro.firebasestorage.app",
-    messagingSenderId: "585595779697",
-    appId: "1:585595779697:web:7729c9357944c12d49f9d0",
-    measurementId: "G-T9P9PB6XH2"
+    apiKey: "AIzaS",
+    authDomain: "sgs",
+    projectId: "sgs-",
+    storageBucket: "sgs-",
+    messagingSenderId: "585595",
+    appId: "1::",
+    measurementId: "G"
 };
 const app = initializeApp(firebaseConfig);
 
